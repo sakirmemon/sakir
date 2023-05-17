8814d75866f9335bc75e7113d5882aec6dbf607d
sakir memon <sakirmemon0507@gmail.com>
2023-05-17 11:24:02 +0530

    new

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 console.log ("hello world")
-let name ="sakir"
 
-console.log (name) 
-var intrestrate = null
-console.log (typeof intrestrate)
 
+let person={
+    name:'sakir', 
+    age:18
+}
+person.name='salim'
+console.log (person.name)
 
 
 
