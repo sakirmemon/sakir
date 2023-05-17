a8d226da8d985d5d525e24f23bcefdb24c0a60a5
sakir memon <sakirmemon0507@gmail.com>
2023-05-17 14:09:41 +0530

    2.10

--- index.js
+++ index.js
@@ -7,7 +7,21 @@ let person={
 }
 person.name='salim'
 console.log (person.name)
-
-
-
-
+let selectedcoulor=['red','blue']
+selectedcoulor[2]='green'
+console.log (selectedcoulor)
+console.log (2+2)
+console.log  (typeof selectedcoulor)
+function sakir (name, lastname) {
+    console.log ('hello ' +  name +  ' '  + lastname)
+}
+sakir ('sakir', 'memon')
+sakir ('salim', 'memon')
+function squer (number) {
+    console.log (number*number)
+}
+squer(2)
+ let x= String(3)
+ console.log (x, typeof x)
+ let v= 'eles'
+ console.log (v)
